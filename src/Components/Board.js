682256e2934bdc5103ae82a6d239ef1fb7c0755b
Abdullahtheauthor <abdullah.goda@vodafone.com>
2024-10-01 22:38:40 +0300

    feat_create_task_button

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,11 +1,22 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 import CreateTask from "./CreateTask";
-
 import "../task.css";
 
 export default function Board() {
+  const navigate = useNavigate(); // Initialize navigate
   const [isCreateTaskOpen, setCreateTaskOpen] = useState(false);
+
+  function handleCancelButtonInCreateTask() {
+    setCreateTaskOpen(false);
+  }
+
+  const handleCreateTaskButton = () => {
+    setCreateTaskOpen(true);
+    // Navigate to /board only when the create task button is clicked
+    navigate("/board");
+  };
+
   return (
     <div style={{ background: "grey", width: "100%", height: "100vh" }}>
       <div style={{ background: "rgb(200,232,232)", width: "100%" }}>
@@ -14,11 +25,11 @@ export default function Board() {
         <button style={{ margin: "20px" }}>Hania</button>
         <button style={{ margin: "20px" }}>Sosusanah</button>
         <button style={{ margin: "20px" }}>Basel</button>
+        <button style={{ margin: "20px" }}>Mohand</button>
+
         <button
           style={{ marginLeft: "100px" }}
-          onClick={() => {
-            setCreateTaskOpen(true);
-          }}
+          onClick={handleCreateTaskButton} // Call this function on click
         >
           Create a task
         </button>
@@ -30,7 +41,12 @@ export default function Board() {
           alignItems: "center",
         }}
       >
-        {isCreateTaskOpen && <CreateTask open={isCreateTaskOpen} />}
+        {isCreateTaskOpen && (
+          <CreateTask
+            open={isCreateTaskOpen}
+            onClose={handleCancelButtonInCreateTask}
+          />
+        )}
       </div>
     </div>
   );
