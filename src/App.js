15eb235277c84f6aeb36f782050e3c3df7a38e10
naierakamel <naierakamel2001@gmail.com>
2024-10-01 21:46:58 +0300

    button

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-<<<<<<< HEAD
-import HomePage from './Pages/HomePage';
-import Navbar from './Components/Navbar';
- import SignupForm from './Components/SignupForm';
- import SigninForm from './Components/SigninForm';
- import GeneralButton from './Components/GeneralButton';
- function App() {
-  return (     
-<BrowserRouter>
-<>
-<Navbar/>
-     <GeneralButton label="Click" />
-     
-
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Signup" element={<SignupForm />} /> 
-          <Route path="/Signin" element={< SigninForm />} /> 
-=======
 import Board from "./Components/Board.js";
-
 import HomePage from "./Pages/HomePage";
 import Navbar from "./Components/Navbar";
 import SignupForm from "./Components/SignupForm.js";
 import SigninForm from "./Components/SigninForm.jsx";
+import GeneralButton from "./Components/GeneralButton";
 
 function App() {
   return (
@@ -34,13 +15,12 @@ function App() {
         {/* <Link to="/Signup">
           <button style={{ fontSize: "20px" }}>SignUpForm</button>
         </Link> */}
-
+<GeneralButton label="click" />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/Signup" element={<SignupForm />} />
           <Route path="/Signin" element={<SigninForm />} />
           <Route path="/board" element={<Board />} />
->>>>>>> a90587e3c41c548e67f1bc90a7d758322d8402a5
         </Routes>
       </>
     </BrowserRouter>
